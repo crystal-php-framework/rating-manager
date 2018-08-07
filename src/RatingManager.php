7cbba9e6e686cd45e09ac7f567ccda7692992877
Nicholas E <isupozoy@gmail.com>
2018-08-07 09:44:03 -0400

    Update RatingManager.php

--- src/RatingManager.php
+++ src/RatingManager.php
@@ -1,1 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Crystal Rating Manager.
+ *
+ * @author Nicholas E. <https://github.com/isupozoy> Maintainer.
+ * @link   <https://github.com/crystal/rating-manager> Github Repository.
+ */
 
+namespace Crystal\Chess;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * The main controller for the ratings.
+ */
+class RatingManager extends Utils implements RatingManager
+{
+}
