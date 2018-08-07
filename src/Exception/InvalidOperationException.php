607e40b181188d4bde95913d8954de45182914f2
Nicholas E <isupozoy@gmail.com>
2018-08-07 08:29:21 -0400

    Update InvalidOperationException.php

--- src/Exception/InvalidOperationException.php
+++ src/Exception/InvalidOperationException.php
@@ -1,1 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Crystal Rating Manager.
+ *
+ * @author Nicholas E. <https://github.com/isupozoy> Maintainer.
+ * @link   <https://github.com/crystal/rating-manager> Github Repository.
+ */
 
+namespace Crystal\Chess\Exception;
+
+/**
+ * If the requested operation is invalid.
+ */
+class InvalidOperationException extends RuntimeException implements ExceptionInterface
+{
+}
