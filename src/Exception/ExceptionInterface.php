e611bca74fcd69690950daa8c34133828fdec748
Nicholas E <isupozoy@gmail.com>
2018-08-07 08:28:18 -0400

    Update ExceptionInterface.php

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,1 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Crystal Rating Manager.
+ *
+ * @author Nicholas E. <https://github.com/isupozoy> Maintainer.
+ * @link   <https://github.com/crystal/rating-manager> Github Repository.
+ */
 
+namespace Crystal\Chess\Exception;
+
+/**
+ * An interface for all exceptions.
+ */
+interface ExceptionInterface extends Throwable
+{
+}
