5dcb63e9caf6a95a397ad11b788bce97630f377a
Nicholas E <isupozoy@gmail.com>
2018-08-07 09:45:02 -0400

    Create RatingInterface.php

---
+++ src/RatingInterface.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Crystal Rating Manager.
+ *
+ * @author Nicholas E. <https://github.com/isupozoy> Maintainer.
+ * @link   <https://github.com/crystal/rating-manager> Github Repository.
+ */
+
+namespace Crystal\Chess;
+
+/**
+ * The interface for all ratings.
+ */
+interface RatingInterface
+{
+}
