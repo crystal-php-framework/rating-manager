9d0688f93e4900a25686cdd2d3aff84e02c11efd
Nicholas E <isupozoy@gmail.com>
2018-08-07 08:30:42 -0400

    Update RuntimeException.php

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -1,1 +1,19 @@
+<?php
+declare(strict_types=1);
+/**
+ * Crystal Rating Manager.
+ *
+ * @author Nicholas E. <https://github.com/isupozoy> Maintainer.
+ * @link   <https://github.com/crystal/rating-manager> Github Repository.
+ */
 
+namespace Crystal\Chess\Exception;
+
+use RuntimeException as GlobalRuntimeException;
+
+/**
+ * Just a base runtime exception.
+ */
+class RuntimeException extends GlobalRuntimeException implements ExceptionInterface
+{
+}
